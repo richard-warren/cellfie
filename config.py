39c42a727a3e53d01ec74d8299a41da5d9e30713
rwarren2163 <richardwarren2163@gmail.com>
2019-10-21 21:18:04 -0400

    wip

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ use_curated_labels = True
 
 # prepare training data
 # datasets = ['N.00.00', 'N.01.01', 'N.02.00', 'N.03.00.t', 'N.04.00.t', 'YST', 'K53', 'J115', 'J123', 'nf.01.00', 'nf.02.01', 'nf.04.01']
-datasets = ['N.00.00']
+datasets = ['K53']
 border_thickness = 2  # (pixels) thickness of borders for border labels
 centroid_radius = 2  # radius of circles at center of neuron mask in target
 summary_frames = 250  # number of frames per batch when computing summary images (1000)
